dba1e584f299681d8c4c1ef96fe5df6d6a91962b
Komal Harshita <komal.sony234@gmail.com>
2025-11-24 22:51:29 +0530

    changes #13

--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,22 @@ import BackToTop from "@/components/back-to-top"
 
 export default function Home() {
   return (
-    <main className="min-h-screen relative">
+    <main className="min-h-screen relative overflow-x-hidden">
     <Navigation />
   
     <GradientMeshBackground />
     <FloatingShapes />
 
-      <Hero />
-      <About />
-      <Projects />
-      <Certifications />
-      <Volunteering />
-      <Contact />
-      <BackToTop />
+      <div className="mx-auto max-w-6xl px-6 sm:px-8 lg:px-12">
+          <Hero />
+          <About />
+          <Projects />
+          <Certifications />
+          <Volunteering />
+          <Contact />
+          <BackToTop />
+        </div>
+        
     </main>
   )
 }
